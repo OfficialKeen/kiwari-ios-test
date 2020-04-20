1a91acce028f464357690869aeb1d7e30bf62162
Keen <zhekeen90@gmail.com>
2020-04-20 21:00:27 +0700

    Initial Commit

---
+++ kiwari-ios-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kiwari-ios-test
+//
+//  Created by keenOI on 20/04/20.
+//  Copyright © 2020 keen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
